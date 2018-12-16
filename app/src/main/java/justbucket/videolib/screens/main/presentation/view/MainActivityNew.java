0f03bad97e6d41097f46572b8471c17146c3d35b
Pliskin Roman <Pliskin.R.O@sberbank.ru>
2018-12-16 09:31:36 +0300

    added permssion request

--- app/src/main/java/justbucket/videolib/screens/main/presentation/view/MainActivityNew.java
+++ app/src/main/java/justbucket/videolib/screens/main/presentation/view/MainActivityNew.java
@@ -1,7 +1,9 @@
 package justbucket.videolib.screens.main.presentation.view;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -59,13 +61,31 @@ public class MainActivityNew extends AppCompatActivity {
         findViewById(R.id.uploadFile).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("image/*");
-                startActivityForResult(intent, READ_REQUEST_CODE);
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            1);
+                }
+                openGallery();
             }
         });
         progressBarContainer = findViewById(R.id.progressBarContainer);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1 && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+            Toast.makeText(this, getString(R.string.toast_permission_denied), Toast.LENGTH_LONG).show();
+            finish();
+        } else {
+            openGallery();
+        }
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(intent, READ_REQUEST_CODE);
     }
 
     @Override
